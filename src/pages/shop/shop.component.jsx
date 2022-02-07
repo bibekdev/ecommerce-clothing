fc6d7b4be821203dbb97301cb00f90dfe526c302
Bibek Chaudhary <kingbibekc@gmail.com>
2022-02-07 13:24:19 +0545

    made few changes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,14 @@
-import React from "react";
+import React from 'react';
+import { Route } from 'react-router-dom';
 
-import ShopData from "./shop.data";
-import CollectionPreview from "../../components/preview-collection/previewCollection.component";
+import CollectionsOverview from '../../components/collections-overview/collectionsOverview.component';
+import CollectionPage from '../collection/collection.component';
 
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: ShopData,
-    };
-  }
-
-  render() {
-    const { collections } = this.state;
-    return (
-      <div className="shop-page">
-        {collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps} />
-        ))}
-      </div>
-    );
-  }
-}
+const ShopPage = ({ match }) => (
+  <div className='shop-page'>
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
 export default ShopPage;
